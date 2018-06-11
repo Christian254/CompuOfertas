39bd2694b50b66e5eb37b25161150e973a7aadb6
Christian254 <christianfuentes254@gmail.com>
2018-06-11 05:06:02 -0600

    listo

--- SIGPAdProject/SIGPAdProject/settings.py
+++ SIGPAdProject/SIGPAdProject/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)  
