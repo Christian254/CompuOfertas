ad739c00a2ea952ebea0d71cd152e8bbfc6cd59a
Christian254 <christianfuentes254@gmail.com>
2018-09-04 00:01:25 -0600

    Pantalla venta
    solo falta el backend xd

--- CompuOfertas/SIGPAdProject/inventario/static/ajax/venta.js
+++ CompuOfertas/SIGPAdProject/inventario/static/ajax/venta.js
@@ -1,47 +1,91 @@
-$(function() {
+$(document).ready(function() {
 	$.ajax({
 		url: 'productoDisponible/',
 		type: 'GET',
 		dataType: 'json',
 	})
-	.done(function(resp) {		
-		resp.forEach( function(prop,index) {
-			var elemento = prop.fields			
-			var producto = '<tr>'
+	.done( function(resp) {
+		for(let i=0;i<resp.length;i++){
+			var elemento = resp[i].fields;
+			var producto = `<tr id="fila${i}">`			
 			producto +=	`<td>${elemento.nombre}</td>`
 			producto +=	`<td>${elemento.marca}</td>`
 			producto +=	`<td>${elemento.existencia}</td>`
-			producto +=	`<td><button id="${prop.pk}" class="btn btn-primary">Añadir</button></td>`
+			producto +=	`<td><button name="${resp[i].pk}" class="agregar btn btn-primary">Añadir</button></td>`
 			producto +=	'</tr>'
 			$('#productoDisponible').append(producto)
+		}
+		var tablaVenta = $('#tablaVenta').DataTable({
+			 lengthMenu: [[2,5,7,-1],["2","5","7","Todos"]],
+    			language: {
+        			"decimal": "",
+        			"emptyTable": "Seleccione los productos a vender",
+        			"info": "",
+        			"infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        			"infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        			"infoPostFix": "",
+        			"thousands": ",",
+        			"lengthMenu": "Mostrar _MENU_ Entradas",
+        			"loadingRecords": "Cargando...",
+        			"processing": "Procesando...",
+        			"search": "Buscar:",
+        			"zeroRecords": "Sin resultados encontrados",
+        			"paginate": {
+            			"first": "Primero",
+            			"last": "Ultimo",
+            			"next": "Siguiente",
+            			"previous": "Anterior"
+        			}
+    			},    
 		});	
-		$('table').DataTable({
-			 lengthMenu: [[1,5,10,-1],["1","5","10","Todos"]],
-    language: {
-        "decimal": "",
-        "emptyTable": "No hay información",
-        "info": "",
-        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-        "infoPostFix": "",
-        "thousands": ",",
-        "lengthMenu": "Mostrar _MENU_ Entradas",
-        "loadingRecords": "Cargando...",
-        "processing": "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords": "Sin resultados encontrados",
-        "paginate": {
-            "first": "Primero",
-            "last": "Ultimo",
-            "next": "Siguiente",
-            "previous": "Anterior"
-        }
-    },
-    
-});	
-	})
+		var tabla = $('#tablaProducto').DataTable({
+			 lengthMenu: [[2,5,7,-1],["2","5","7","Todos"]],
+    			language: {
+        			"decimal": "",
+        			"emptyTable": "No hay productos para vender",
+        			"info": "",
+        			"infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        			"infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        			"infoPostFix": "",
+        			"thousands": ",",
+        			"lengthMenu": "Mostrar _MENU_ Entradas",
+        			"loadingRecords": "Cargando...",
+        			"processing": "Procesando...",
+        			"search": "Buscar:",
+        			"zeroRecords": "Sin resultados encontrados",
+        			"paginate": {
+            			"first": "Primero",
+            			"last": "Ultimo",
+            			"next": "Siguiente",
+            			"previous": "Anterior"
+        			}
+    			},    
+		});
+		$('#tablaProducto tbody').on( 'click', '.agregar', function () {
+    	let fila = tabla.row( $(this).parents('tr') );
+    	let productoDatos = tabla.row( $(this).parents('tr') ).data();
+    	console.log(productoDatos);
+        fila.remove();
+        tabla.draw();
+        tablaVenta.row.add(
+        	[productoDatos[0], 
+        	productoDatos[1],productoDatos[2],'<input type="text">','<button type="button" class="btn btn-danger quitar">Quitar</button>']).draw();			
+		});
+		$('#tablaVenta tbody').on( 'click', '.quitar', function () {
+    	let fila = tablaVenta.row( $(this).parents('tr') );
+    	let productoDatos = tablaVenta.row( $(this).parents('tr') ).data();
+    	console.log(productoDatos);
+        fila.remove();
+        tablaVenta.draw();
+        tabla.row.add(
+        	[productoDatos[0], 
+        	productoDatos[1],productoDatos[2],'<button type="button" class="agregar btn btn-primary">Añadir</button>']).draw();			
+		});
+		})
 	.fail(function() {
 		console.log("error");
 	})
-	
 })
+	
+
+
